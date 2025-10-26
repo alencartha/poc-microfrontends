30ffceca50581c663655b7a0664c9dc57bdcf43f
alencartha <alencar_tha@hotmail.com>
2025-10-26 17:29:23 -0300

    refact: ajuste workflow

--- mfe-produtos/webpack.config.js
+++ mfe-produtos/webpack.config.js
@@ -1,18 +1,41 @@
+// mfe-produtos/webpack.config.js
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { withModuleFederation } = require('@angular-architects/module-federation/webpack');
 
 module.exports = async (config, context) => {
+  // Cria a configuração de Module Federation
   const mf = await withModuleFederation({
     name: 'mfeProdutos',
     filename: 'remoteEntry.js',
     exposes: {
-      './ProdutosComponent': './src/app/produtos/produtos.component.ts'
+      // Ajuste o caminho se seu componente mudou de lugar
+      './ProdutosComponent': './src/app/produtos/produtos.component.ts',
     },
     shared: {
-      '@angular/core': { singleton: true, strictVersion: true },
-      '@angular/common': { singleton: true, strictVersion: true },
-      '@angular/router': { singleton: true, strictVersion: true }
-    }
+      '@angular/core':   { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+      '@angular/common': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+      '@angular/router': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+      'rxjs':            { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+      'zone.js':         { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    },
   });
 
-  return mf(config);
+  // Mescla com a config do Angular
+  const merged = mf(config);
+
+  // Boas práticas para MF com Angular 16+ (vale para 20):
+  merged.output = {
+    ...(merged.output || {}),
+    uniqueName: 'mfeProdutos',
+    publicPath: 'auto',
+    clean: true,
+  };
+
+  // Evita múltiplos runtimes quando for carregado pelo host
+  merged.optimization = {
+    ...(merged.optimization || {}),
+    runtimeChunk: false,
+  };
+
+  return merged;
 };
